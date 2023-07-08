89ba7a8c3756d916548ed1e82f7f05ff3c5c34df
Chris Campbell <chrs.cmpbl@gmail.com>
2023-07-08 13:32:06 -0600

    add tailwind

---
+++ admin/app/src/views/AddView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const checked = ref(true)
+const name = ref('')
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold underline">
+      Add a Strain
+    </h1>
+
+    <label for="name">Name</label>
+    <input v-model="name" placeholder="Name" />
+
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">{{ checked }}</label>
+  </main>
+</template>
