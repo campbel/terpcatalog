1353197b247338065e62463631a31b6f6b57a81e
Chris Campbell <chrs.cmpbl@gmail.com>
2023-07-07 23:56:12 -0600

    simple fileserver

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 
 	"github.com/campbel/terpcatalog/util/config"
 	"github.com/campbel/terpcatalog/util/log"
 )
 
+//go:embed public
+var public embed.FS
+
 func main() {
 	log.Info("starting server", "port", config.Port())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
+	fsys, err := fs.Sub(public, "public")
+	if err != nil {
+		log.FatalError("error during fs.Sub", err)
+	}
 
+	http.Handle("/", http.FileServer(http.FS(fsys)))
 	if err := http.ListenAndServe(":"+config.Port(), nil); err != nil {
 		log.FatalError("error during http.ListenAndServe", err)
 	}
