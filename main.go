36be631d19684eb5a55050d242d6c7dbab3e1752
Chris Campbell <chrs.cmpbl@gmail.com>
2023-07-08 00:09:23 -0600

    setup and run

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/campbel/terpcatalog/util/config"
 	"github.com/campbel/terpcatalog/util/log"
@@ -13,14 +16,30 @@ import (
 var public embed.FS
 
 func main() {
-	log.Info("starting server", "port", config.Port())
+	log.Info("starting server...", "port", config.Port())
 	fsys, err := fs.Sub(public, "public")
 	if err != nil {
 		log.FatalError("error during fs.Sub", err)
 	}
 
 	http.Handle("/", http.FileServer(http.FS(fsys)))
-	if err := http.ListenAndServe(":"+config.Port(), nil); err != nil {
-		log.FatalError("error during http.ListenAndServe", err)
-	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool, 1)
+
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	go func() {
+		if err := http.ListenAndServe(":"+config.Port(), nil); err != nil {
+			log.FatalError("error during http.ListenAndServe", err)
+		}
+		done <- true
+	}()
+
+	<-done
+	log.Info("shutting down...")
 }
